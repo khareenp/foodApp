e4d75517d14e24a7271c317c94885eeec786c867
khareenp <khareenp1@gmail.com>
2023-04-11 02:41:56 -0400

    add app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
